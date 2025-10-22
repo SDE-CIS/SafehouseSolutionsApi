a98ff9ac058fa3ed426230e8eef6ff4545562db7
Harry Warden <ivan2346@edu.sde.dk>
2025-10-22 12:58:56 +0200

    fix fejlbesked

--- src/controllers/keycardController.js
+++ src/controllers/keycardController.js
@@ -68,7 +68,7 @@ export const getKeycardsStatusesByID = async (req, res) => {
     try {
         const keycardStatusQuery = `SELECT * FROM StatusTypes WHERE ID = @ID`;
         const result = await executeQuery(keycardStatusQuery, [{ name: 'ID', value: id }]);
-        if (result.recordset.length === 0) { return res.status(404).json({ success: false, message: 'Unit not found' }); }
+        if (result.recordset.length === 0) { return res.status(404).json({ success: false, message: 'Status type not found.' }); }
         res.status(200).json({ success: true, data: result.recordset[0] });
     } catch (error) {
         console.error('Connection error:', error);
