3ffcdf5e5294c9ca53281277800c34200a546729
Ivana Vasileva <82370466+ivan2346@users.noreply.github.com>
2025-10-22 10:27:31 +0200

    Delete src/routes/userAccounts.js

--- src/routes/userAccounts.js
+++
@@ -1,7 +0,0 @@
-import Router from 'express';
-import { getUserAccounts } from '../controllers/userAccountsController.js';
-const router = Router();
-
-router.get('/', getUserAccounts);
-
-export default router;
