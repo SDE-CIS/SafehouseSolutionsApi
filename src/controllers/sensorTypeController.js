e3b1dd4a4673701190763f8245c8bd353f246b2c
Harry Warden <ivan2346@edu.sde.dk>
2025-10-22 08:56:01 +0200

    db navnet var FORKERT

--- src/controllers/sensorTypeController.js
+++ src/controllers/sensorTypeController.js
@@ -3,7 +3,7 @@ import { executeQuery } from '../utils/executeQuery.js';
 // GET /sensor/type
 export const getSensorTypes = async (req, res) => {
     try {
-        const sensorTypesQuery = `SELECT * FROM SensorTypes`;
+        const sensorTypesQuery = `SELECT * FROM SensorType`;
         const result = await executeQuery(sensorTypesQuery);
         res.status(200).json({ success: true, data: result.recordset });
     } catch (error) {
@@ -16,8 +16,8 @@ export const getSensorTypes = async (req, res) => {
 export const getSensorTypesByID = async (req, res) => {
     const { id } = req.params;
     try {
-        const sensorType = `SELECT * FROM SensorTypes WHERE ID = @ID`;
-        const result = await executeQuery(sensorType, [{ name: 'ID', value: id }]);
+        const sensorTypeQuery = `SELECT * FROM SensorType WHERE ID = @ID`;
+        const result = await executeQuery(sensorTypeQuery, [{ name: 'ID', value: id }]);
         if (result.recordset.length === 0) { return res.status(404).json({ success: false, message: 'Unit not found' }); }
         res.status(200).json({ success: true, data: result.recordset[0] });
     } catch (error) {
--- src/routes/sensorType.js
+++ src/routes/sensorType.js
@@ -2,7 +2,7 @@ import Router from 'express';
 import { getSensorTypes, getSensorTypesByID } from '../controllers/sensorTypeController.js';
 const router = Router();
 
-router.get('', getSensorTypes);
-router.get('/:id', getSensorTypesByID);
+router.get('/type', getSensorTypes);
+router.get('/type/:id', getSensorTypesByID);
 
 export default router;
