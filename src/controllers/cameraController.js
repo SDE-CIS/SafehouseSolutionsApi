2a3f876a1864004bd9886230126c06ac9b3ea97d
hfgf-csc <c.spillehus@live.dk>
2025-11-12 10:36:42 +0100

    camaera data endpoint fix

--- src/controllers/cameraController.js
+++ src/controllers/cameraController.js
@@ -20,7 +20,7 @@ export const getCameraDataByID = async (req, res) => {
         const result = await executeQuery(query, [{ name: "DeviceID", value: id }]);
         if (result.recordset.length === 0)
             return res.status(404).json({ success: false, message: 'Camera Data not found.' });
-        res.status(200).json({ success: true, data: result.recordset[0] })
+        res.status(200).json({ success: true, data: result.recordset })
     } catch (error) {
         res.status(500).json({ success: false, message: error.message || error });
     }
