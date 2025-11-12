516337ea1b23b9d8a444d4fd3d8e3970fdcab14a
hfgf-csc <c.spillehus@live.dk>
2025-11-12 08:50:50 +0100

    camera data by id

--- src/controllers/cameraController.js
+++ src/controllers/cameraController.js
@@ -10,7 +10,20 @@ export const getCameraData = async (req, res) => {
 
 // GET /camera/:id
 export const getCameraDataByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (id == undefined) {
+            return res.status(400).json({ success: false, message: 'id must be defined.' });
+        }
 
+        const query = `SELECT * FROM CameraData WHERE DeviceID = @DeviceID`;
+        const result = await executeQuery(query, [{ name: "DeviceID", value: id }]);
+        if (result.recordset.length === 0)
+            return res.status(404).json({ success: false, message: 'Camera Data not found.' });
+        res.status(200).json({ success: true, data: result.recordset[0] })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message || error });
+    }
 };
 
 // POST /camera
